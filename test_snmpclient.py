5dcb609461abb0a7a71ddfe23bb85e17d622b9fb
Robert Wlodarczyk <rwlodarc@impinj.com>
2015-09-24 10:41:15 -0700

    Fixing PEP8 violation.

--- test_snmpclient.py
+++ test_snmpclient.py
@@ -9,4 +9,7 @@ client = snmpclient.SnmpClient(router, port, public_auth, private_auth)
 print client.alive
 print client.get('SNMPv2-MIB::sysName.0')
 print client.gettable('UDP-MIB::udpLocalAddress')
-print client.matchtables('IF-MIB::ifIndex', ('IF-MIB::ifDescr', 'IF-MIB::ifPhysAddress', 'IF-MIB::ifOperStatus'))
+print client.matchtables('IF-MIB::ifIndex',
+                         ('IF-MIB::ifDescr',
+                          'IF-MIB::ifPhysAddress',
+                          'IF-MIB::ifOperStatus'))
