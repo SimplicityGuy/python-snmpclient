7806aa260b5ba86d2e9ab0f85682ca6b7323796b
Robert Wlodarczyk <rwlodarc@impinj.com>
2015-09-24 10:35:37 -0700

    Fixing spelling mistake.

--- test_snmpclient.py
+++ test_snmpclient.py
@@ -3,9 +3,9 @@ import snmpclient
 router = '10.100.184.62'
 port = 161
 public_auth = [{'community': 'public', 'version': snmpclient.V2C}]
-priavte_auth = [{'community': 'private', 'version': snmpclient.V2C}]
+private_auth = [{'community': 'private', 'version': snmpclient.V2C}]
 
-client = snmpclient.SnmpClient(router, port, public_auth, priavte_auth)
+client = snmpclient.SnmpClient(router, port, public_auth, private_auth)
 print client.alive
 print client.get('SNMPv2-MIB::sysName.0')
 print client.gettable('UDP-MIB::udpLocalAddress')
