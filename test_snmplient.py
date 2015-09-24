cce48368e33ac879bad181a915ace5ad0d872979
Robert Wlodarczyk <rwlodarc@impinj.com>
2015-09-24 08:58:32 -0700

    Fixing the test case.

--- test_snmplient.py
+++ test_snmplient.py
@@ -1,9 +1,11 @@
 import snmpclient
 
-router = '10.42.1.1'
-authdata = {'community': 'public', 'version': snmpclient.V2C}
+router = '10.100.184.62'
+port = 161
+authdata = [{'community': 'public', 'version': snmpclient.V2C},
+            {'community': 'private', 'version': snmpclient.V2C}]
 
-client = snmpclient.SnmpClient(router, [authdata])
+client = snmpclient.SnmpClient(router, port, authdata)
 print client.alive
 print client.get('SNMPv2-MIB::sysName.0')
 print client.gettable('UDP-MIB::udpLocalAddress')
